57f06008e57b58918755dd203cdbdb92d1bd5c32
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2017-04-21 17:00:53 +0200

    modularize pom generator

--- genfeature-maven-plugin/src/main/java/org/ietr/maven/genfeature/PomGenerator.java
+++ genfeature-maven-plugin/src/main/java/org/ietr/maven/genfeature/PomGenerator.java
@@ -1,89 +1,120 @@
 package org.ietr.maven.genfeature;
 
-import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
-
 public class PomGenerator {
 
-  public static final String generateSitePomFile(final GenerateAllInOneP2Feature generateAllInOneP2Feature)
-      throws FileNotFoundException, UnsupportedEncodingException {
-    final StringBuilder buffer = new StringBuilder();
+  private static final String TYCHO_GROUP_ID = "org.eclipse.tycho";
 
-    buffer.append("<project xmlns=\"http://maven.apache.org/POM/4.0.0\" ");
-    buffer.append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
-    buffer.append("xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n");
-    buffer.append("  <modelVersion>4.0.0</modelVersion>\n");
-    buffer.append("  <artifactId>org.ietr.externaldeps.dependency.site</artifactId>\n");
-    buffer.append("  <packaging>eclipse-repository</packaging>\n");
-    buffer.append("  \n");
-    buffer.append("  <parent>\n");
-    buffer.append("    <artifactId>" + generateAllInOneP2Feature.project.getGroupId() + ".parent</artifactId>\n");
-    buffer.append("    <groupId>" + generateAllInOneP2Feature.project.getGroupId() + "</groupId>\n");
-    buffer.append("    <version>" + generateAllInOneP2Feature.project.getVersion() + "</version>\n");
-    buffer.append("    <relativePath>..</relativePath>\n");
-    buffer.append("  </parent>\n");
-    buffer.append("  \n");
-    buffer.append("  <build>\n");
-    buffer.append("    <plugins>\n");
-    buffer.append("      <plugin>\n");
-    buffer.append("        <groupId>org.eclipse.tycho</groupId>\n");
-    buffer.append("        <artifactId>tycho-maven-plugin</artifactId>\n");
-    buffer.append("        <version>" + GenerateAllInOneP2Feature.TYCHO_VERSION + "</version>\n");
-    buffer.append("        <extensions>true</extensions>\n");
-    buffer.append("      </plugin>\n");
-    buffer.append("      <plugin>\n");
-    buffer.append("        <groupId>org.eclipse.tycho</groupId>\n");
-    buffer.append("        <artifactId>tycho-p2-repository-plugin</artifactId>\n");
-    buffer.append("        <version>" + GenerateAllInOneP2Feature.TYCHO_VERSION + "</version>\n");
-    buffer.append("        <configuration>\n");
-    buffer.append("          <includeAllDependencies>true</includeAllDependencies>\n");
-    buffer.append("          <compress>false</compress>\n");
-    buffer.append("          <repositoryName>" + generateAllInOneP2Feature.featureProvider + " Update Site</repositoryName>\n");
-    buffer.append("        </configuration>\n");
-    buffer.append("      </plugin>\n");
-    buffer.append("    </plugins>\n");
-    buffer.append("  </build>\n");
-    buffer.append("</project>\n");
-    buffer.append("\n");
-    buffer.append("");
+  private PomGenerator() {
+  }
+
+  public static final String generateFeaturePomFile(final GenerateAllInOneP2Feature generateAllInOneP2Feature) {
+    final StringBuilder buffer = new StringBuilder();
+    PomGenerator.headers(buffer);
+    PomGenerator.artifactId(generateAllInOneP2Feature.project.getGroupId() + "." + generateAllInOneP2Feature.featureId, buffer);
+    PomGenerator.packaging("eclipse-feature", buffer);
+
+    PomGenerator.parent(generateAllInOneP2Feature.project.getGroupId() + ".parent", generateAllInOneP2Feature.project.getGroupId(),
+        generateAllInOneP2Feature.project.getVersion(), "..", buffer);
+
+    startPlugins(buffer);
+    plugin("tycho-maven-plugin", TYCHO_GROUP_ID, GenerateAllInOneP2Feature.TYCHO_VERSION, "<extensions>true</extensions>", buffer);
+    endPlugins(buffer);
+
+    PomGenerator.footer(buffer);
 
     return buffer.toString();
   }
 
-  public static final String generateFeaturePomFile(final GenerateAllInOneP2Feature generateAllInOneP2Feature)
-      throws FileNotFoundException, UnsupportedEncodingException {
-    final StringBuilder buffer = new StringBuilder();
-
-    buffer.append("<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n");
-    buffer.append("  xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n");
-    buffer.append("  <modelVersion>4.0.0</modelVersion>\n");
-    buffer.append("\n");
-    buffer.append("  <artifactId>");
-    buffer.append(generateAllInOneP2Feature.project.getGroupId() + ".");
-    buffer.append(generateAllInOneP2Feature.featureId);
-    buffer.append("</artifactId>\n");
-    buffer.append("  <packaging>eclipse-feature</packaging>\n");
-    buffer.append("  \n");
-    buffer.append("  <parent>\n");
-    buffer.append("    <artifactId>" + generateAllInOneP2Feature.project.getGroupId() + ".parent</artifactId>\n");
-    buffer.append("    <groupId>" + generateAllInOneP2Feature.project.getGroupId() + "</groupId>\n");
-    buffer.append("    <version>" + generateAllInOneP2Feature.project.getVersion() + "</version>\n");
-    buffer.append("    <relativePath>..</relativePath>\n");
-    buffer.append("  </parent>\n");
-    buffer.append("  \n");
-    buffer.append("  <build>\n");
-    buffer.append("    <plugins>\n");
-    buffer.append("      <plugin>\n");
-    buffer.append("        <groupId>org.eclipse.tycho</groupId>\n");
-    buffer.append("        <artifactId>tycho-maven-plugin</artifactId>\n");
-    buffer.append("        <version>" + GenerateAllInOneP2Feature.TYCHO_VERSION + "</version>\n");
-    buffer.append("        <extensions>true</extensions>\n");
-    buffer.append("      </plugin>\n");
+  private static void endPlugins(StringBuilder buffer) {
     buffer.append("    </plugins>\n");
     buffer.append("  </build>\n");
-    buffer.append("</project>\n");
-    buffer.append("");
+  }
+
+  private static void startPlugins(StringBuilder buffer) {
+    buffer.append("  <build>\n");
+    buffer.append("    <plugins>\n");
+  }
+
+  public static final String generateSitePomFile(final GenerateAllInOneP2Feature generateAllInOneP2Feature) {
+    final StringBuilder buffer = new StringBuilder();
+
+    PomGenerator.headers(buffer);
+    PomGenerator.artifactId("org.ietr.externaldeps.dependency.site", buffer);
+    PomGenerator.packaging("eclipse-repository", buffer);
+    PomGenerator.parent(generateAllInOneP2Feature.project.getGroupId() + ".parent", generateAllInOneP2Feature.project.getGroupId(),
+        generateAllInOneP2Feature.project.getVersion(), "..", buffer);
+
+    startPlugins(buffer);
+
+    plugin("tycho-maven-plugin", TYCHO_GROUP_ID, GenerateAllInOneP2Feature.TYCHO_VERSION, "<extensions>true</extensions>", buffer);
+
+    String extraConfig = "<configuration>\n" + "<includeAllDependencies>true</includeAllDependencies>\n" + "<compress>false</compress>\n" + "<repositoryName>"
+        + generateAllInOneP2Feature.featureProvider + " Update Site</repositoryName>\n" + "</configuration>\n";
+    plugin("tycho-p2-repository-plugin", TYCHO_GROUP_ID, GenerateAllInOneP2Feature.TYCHO_VERSION, extraConfig, buffer);
+
+    endPlugins(buffer);
+
+    PomGenerator.footer(buffer);
 
     return buffer.toString();
+  }
+
+  private static final void headers(final StringBuilder buffer) {
+    buffer.append("<project xmlns=\"http://maven.apache.org/POM/4.0.0\" ");
+    buffer.append("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" ");
+    buffer.append("xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n");
+    buffer.append("<modelVersion>4.0.0</modelVersion>\n");
+    buffer.append("\n");
+  }
+
+  private static void footer(final StringBuilder buffer) {
+    buffer.append("</project>\n");
+    buffer.append("\n");
+    buffer.append("");
+  }
+
+  private static final void plugin(final String artifactId, final String groupId, final String version, final String extraConfig, final StringBuilder buffer) {
+
+    buffer.append("<plugin>\n");
+    groupId(groupId, buffer);
+    artifactId(artifactId, buffer);
+    version(version, buffer);
+    buffer.append(extraConfig + "\n");
+    buffer.append("</plugin>\n");
+  }
+
+  private static void packaging(final String packagingType, final StringBuilder buffer) {
+    buffer.append("<packaging>");
+    buffer.append(packagingType);
+    buffer.append("</packaging>\n");
+    buffer.append("\n");
+  }
+
+  private static void parent(final String artifactId, final String groupId, final String version, final String relativePath, final StringBuilder buffer) {
+    buffer.append("<parent>\n");
+
+    PomGenerator.artifactId(artifactId, buffer);
+    PomGenerator.groupId(groupId, buffer);
+    PomGenerator.version(version, buffer);
+    PomGenerator.relativePath(relativePath, buffer);
+
+    buffer.append("</parent>\n");
+    buffer.append("\n");
+  }
+
+  private static void relativePath(final String relativePath, final StringBuilder buffer) {
+    buffer.append("<relativePath>" + relativePath + "</relativePath>\n");
+  }
+
+  private static void version(final String version, final StringBuilder buffer) {
+    buffer.append("<version>" + version + "</version>\n");
+  }
+
+  private static void groupId(final String groupId, final StringBuilder buffer) {
+    buffer.append("<groupId>" + groupId + "</groupId>\n");
+  }
+
+  private static void artifactId(final String artifactId, final StringBuilder buffer) {
+    buffer.append("<artifactId>" + artifactId + "</artifactId>\n");
   }
 }
